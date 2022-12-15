0398f5db7ad61a8e225b969f6c6e676de03f79e3
bbbbbr <bbbbbr@users.noreply.github.com>
2022-12-14 22:48:48 -0800

    Tools: DevSound X, more macros to copy & paste C into JS (not sure it's worth it)

--- js/mem_search.js
+++ js/mem_search.js
@@ -99,3 +99,36 @@ function FORMAT_ENTRY(e_type, e_name, e_version) {
 function CHECK_PATTERN_AT_ADDR(u8PatternBuf, check_addr) {
     return checkPatternAtAddr_u8(memsearch_u8RomBuffer, u8PatternBuf, check_addr);
 }
+
+
+// To avoid having to pass the buffer in for each search,
+// allow calling function to set a local version.
+function set_memsearch_u8RomBuffer(u8RomBuffer) {
+    memsearch_u8RomBuffer = u8RomBuffer;
+}
+
+
+function window_add_property(prop_name, prop_value){
+  Object.defineProperty(window, prop_name, {
+    value: prop_value,
+    configurable: false,
+    writable: false
+  });
+}
+
+function DEF_PATTERN_STR(varname, str_content) {
+
+    let utf8Encoder = new TextEncoder();
+    // Equiv example: const sig_str_devsound_lite = utf8Encoder.encode("DevSound Lite");
+    window_add_property(varname, utf8Encoder.encode(str_content));
+}
+
+function AR_ARGS() {
+    return new Uint8Array ([...arguments]);
+}
+
+function DEF_PATTERN_BUF(varname, buf_content) {
+
+    // Equiv example: const sig_gbsoundsystem_MultiSFXLoop = new Uint8Array([0x2A, 0x4E, 0x06, 0x00, 0x87, 0xCB, 0x10, 0x87, 0xCB, 0x10, 0x87, 0xCB, 0x10]);
+    window_add_property(varname, buf_content);
+}
